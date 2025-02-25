291febbad867751f3f7f504042935d5c837fc443
EONCreator <ITCyborg2001@gmail.com>
2025-02-25 12:04:22 +0300

    redoc added, .env added

--- database/database.py
+++ database/database.py
@@ -1,21 +1,42 @@
+import os
+from dotenv import load_dotenv
+
+import asyncpg
 from sqlalchemy.ext.asyncio import create_async_engine, async_session, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/blog"
+load_dotenv()
+
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_URL = f"postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
 
-
 Base = declarative_base()
 
 
+async def create_database_if_not_exists():
+    print("Creating database...")
+    conn = await asyncpg.connect(user=DATABASE_USER, password=DATABASE_PASSWORD, host=DATABASE_HOST, port=DATABASE_PORT)
+    try:
+        databases = await conn.fetch("SELECT datname FROM pg_database;")
+        if DATABASE_NAME not in [db['datname'] for db in databases]:
+            await conn.execute(f"CREATE DATABASE {DATABASE_NAME}")
+    finally:
+        await conn.close()
+
+
 async def init_models():
+    print("Creating tables...")
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
--- main.py
+++ main.py
@@ -3,24 +3,45 @@ from fastapi import FastAPI
 import typer
 import asyncio
 
-from database.database import init_models
+from fastapi.openapi.utils import get_openapi
+
+from database.database import init_models, create_database_if_not_exists
 from api.routers.routers import all_routers
 
-cli = typer.Typer()
-
-@cli.command()
-def db_init_models():
-    asyncio.run(init_models())
-    print("Done")
-    
-
 app = FastAPI(
-    title="API Блога"
+    swagger_ui_parameters={"syntaxHighlight": False},
+    title="Blog API"
 )
 
 for router in all_routers:
     app.include_router(router)
 
+
+@app.on_event("startup")
+async def startup_event():
+    print("Initializing database...")
+    await create_database_if_not_exists()
+    await init_models()
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Blog API",
+        version="0.0.1",
+        description="API for Blog",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
 if __name__ == "__main__":
-    cli()
     uvicorn.run(app, host="0.0.0.0", port=8000)
